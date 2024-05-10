5d6c2456abb5781f97af39bb6fa7ee57b112e50b
rafaelarbz <rafaela.souza@artemis5.com.br>
2024-05-09 22:18:43 -0300

    fix ident

--- src/components/general/ConfirmDialogComponent.jsx
+++ src/components/general/ConfirmDialogComponent.jsx
@@ -4,42 +4,41 @@ import { ConfirmDialog } from 'primereact/confirmdialog';
 export default function ConfirmDialogComponent({visible, icon="pi pi-question", title="Atenção", message, onConfirm, onReject}) {
     return (
         <ConfirmDialog
-                group="headless"
-                visible={visible}
-                content={() => (
-                    <div className="flex flex-column align-items-center p-5 surface-overlay border-round">
-                        <div className="border-circle bg-primary inline-flex justify-content-center align-items-center h-6rem w-6rem -mt-8">
-                            <i className={`${icon} text-5xl`}></i>
-                        </div>
-                        <span className="font-bold text-2xl block mb-2 mt-4">
-                            {title}
-                        </span>
-                        <p className="mb-0" >
-                            {message}
-                        </p>
-                        <div className="flex align-items-center gap-2 mt-4" >
-                            <Button
-                                label="Confirmar"
-                                text raised
-                                severity="success"
-                                onClick={() => {
-                                    onConfirm()
-                                }}
-                                className="w-8rem"
-                            ></Button>
-                            <Button
-                                label="Cancelar"
-                                text raised
-                                severity="danger"
-                                onClick={() => {
-                                    onReject()
-                                }}
-                                className="w-8rem"
-                            ></Button>
-                        </div>
+            group="headless"
+            visible={visible}
+            content={() => (
+                <div className="flex flex-column align-items-center p-5 surface-overlay border-round">
+                    <div className="border-circle bg-primary inline-flex justify-content-center align-items-center h-6rem w-6rem -mt-8">
+                        <i className={`${icon} text-5xl`}></i>
                     </div>
-                )}
-                
-            />
+                    <span className="font-bold text-2xl block mb-2 mt-4">
+                        {title}
+                    </span>
+                    <p className="mb-0" >
+                        {message}
+                    </p>
+                    <div className="flex align-items-center gap-2 mt-4" >
+                        <Button
+                            label="Confirmar"
+                            text raised
+                            severity="success"
+                            onClick={() => {
+                                onConfirm()
+                            }}
+                            className="w-8rem"
+                        ></Button>
+                        <Button
+                            label="Cancelar"
+                            text raised
+                            severity="danger"
+                            onClick={() => {
+                                onReject()
+                            }}
+                            className="w-8rem"
+                        ></Button>
+                    </div>
+                </div>
+            )}
+        />
     );
 }
