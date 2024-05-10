7614ab4103538ce06c8269a6132b02838668c8ed
rafaelarbz <rafaela.souza@artemis5.com.br>
2024-05-09 23:44:16 -0300

    add home option on sidebar

--- src/components/general/SidebarComponent.jsx
+++ src/components/general/SidebarComponent.jsx
@@ -27,6 +27,15 @@ export default function SidebarComponent() {
         <ul className="list-none p-0 m-0 overflow-hidden">
           <li>
             <a 
+              href="/"
+              className="p-ripple flex align-items-center cursor-pointer p-3 border-round text-700 hover:surface-100 transition-duration-150 transition-colors w-full no-underline">
+              <i className="pi pi-home mr-2"></i>
+              <span>Home</span>
+              <Ripple />
+            </a>
+          </li>
+          <li>
+            <a 
               href="/new"
               className="p-ripple flex align-items-center cursor-pointer p-3 border-round text-700 hover:surface-100 transition-duration-150 transition-colors w-full no-underline">
               <i className="pi pi-plus mr-2"></i>
