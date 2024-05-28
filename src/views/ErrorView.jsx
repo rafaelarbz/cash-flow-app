846f36f3cb846915728ca06db9494a3e10a3fd3d
rafaelarbz <rafaelarabeloeng@gmail.com>
2024-05-28 17:20:48 -0300

    add folder for images

--- src/views/ErrorView.jsx
+++ src/views/ErrorView.jsx
@@ -5,7 +5,7 @@ export default function ErrorView() {
         <div className="flex justify-content-center mt-8">
             <BasicCardComponent 
                 title="Ooops"
-                content={ <img src="not-found.svg" className="h-20rem w-20rem" />}
+                content={ <img src="images/not-found.svg" className="h-20rem w-20rem" />}
             />
         </div>
     );
